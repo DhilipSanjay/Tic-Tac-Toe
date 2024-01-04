3cd17dd213af9b067cf124287e223a5b898d9799
Vladimir Stoyanov <vladisto100@gmail.com>
2024-01-05 00:10:02 +0200

    terminal mode added

---
+++ client-terminal/terminal-player.py
@@ -0,0 +1,81 @@
+import socket
+import sys
+import time
+
+s = socket.socket()
+if len(sys.argv) > 1:
+    host = sys.argv[1]
+else:
+    host = "localhost"
+port = 9999
+
+
+def print_matrix(matrix):
+    # print(matrix)
+    for i in range(3):
+        for j in range(3):
+            current = "_"
+            if matrix[i][j] == 1:
+                current = "X"
+            elif matrix[i][j] == 2:
+                current = "O"
+            print(current, end="\t")
+        print("")
+
+
+def start_player():
+    try:
+        s.connect((host, port))
+        print("Connected to :", host, ":", port)
+        start_game()
+        s.close()
+    except socket.error as e:
+        print("Socket connection error:", e)
+
+
+def start_game():
+    welcome = s.recv(2048 * 10)
+    print(welcome.decode())
+
+    name = input("Enter Player name:")
+    s.send(name.encode())
+
+    while True:
+        try:
+            recvData = s.recv(2048 * 10)
+            recvDataDecode = recvData.decode()
+
+            if recvDataDecode == "Input":
+                failed = 1
+                while failed:
+                    try:
+                        x = int(input("Enter the x coordinate:"))
+                        y = int(input("Enter the y coordinate:"))
+                        coordinates = str(x) + "," + str(y)
+                        s.send(coordinates.encode())
+                        failed = 0
+                    except:
+                        print("Error occured....Try again")
+
+            elif recvDataDecode == "Error":
+                print("Error occured! Try again..")
+
+            elif recvDataDecode == "Matrix":
+                print(recvDataDecode)
+                matrixRecv = s.recv(2048 * 100)
+                matrixRecvDecoded = matrixRecv.decode("utf-8")
+                print_matrix(eval(matrixRecvDecoded))
+
+            elif recvDataDecode == "":
+                time.sleep(10)
+                break
+
+            else:
+                print(recvDataDecode)
+        except KeyboardInterrupt:
+            print("\nKeyboard Interrupt")
+            time.sleep(1)
+            break
+
+
+start_player()
